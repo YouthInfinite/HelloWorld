c7c8238974ed4d566c7c1ce7bb09a3d292a74189
Thyme7 <755821662@qq.com>
2021-10-06 18:03:07 +0800

    idea 集成 git  第三次提交测试

--- src/main/java/com/thyme7/HelloWorld.java
+++ src/main/java/com/thyme7/HelloWorld.java
@@ -10,5 +10,6 @@ public class HelloWorld
     {
         System.out.println( "Hello World!" );
         System.out.println( "Hello World! ---2" );
+        System.out.println( "Hello World! ---master" );
     }
 }
